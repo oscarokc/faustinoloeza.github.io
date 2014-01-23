9c41d883e08429f761bc028b04dac73aa441c6f1
Faustino Loeza <fausto_zair@hotmail.com>
2014-01-23 11:17:51 -0600

    faustin

---
+++ js/funciones.js
@@ -0,0 +1,37 @@
+/*Funciones 
+autor: Faustino Loeza Perez
+fecha: 09/2013
+*/
+x=$(document);
+    x.ready(girarimagenes);
+    x.ready(pop);
+    x.ready(menu);
+     
+     function girarimagenes()
+     {
+          x=$('ul#imagenes');
+          x.roundabout({btnNext: ".next", duration: 450,reflect:true, btnPrev: ".prev",duration: 450, reflect: false,autoplay: true,autoplayDuration: 5000});
+     }
+     function menu ()
+     {
+        var x=$("#menu");
+          x.click(menumagico);
+     }
+     function menumagico()
+     {
+        var x=$("nav ul");
+        x.slideToggle("slow");
+     }
+
+ 
+       function pop()
+        {
+            var x=$("#boton");
+                x.click(ver);
+        }   
+    
+
+        function ver ()
+        {
+            $("#tarjetas").slideToggle("slow");
+        }
