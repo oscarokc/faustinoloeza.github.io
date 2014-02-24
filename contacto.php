39e9745b820fb064646cf084e13f1413ebd07c0c
Faustino Loeza <fausto_zair@hotmail.com>
2014-02-24 10:18:56 -0600

    forimulario

--- contacto.php
+++ contacto.php
@@ -1,29 +1,30 @@
 <?php
 //Importamos las variables del formulario de contacto
-
 @$nombre = addslashes($_POST['nombre']);
 @$email = addslashes($_POST['email']);
+@$web = addslashes($_POST['web']);
 @$mensaje = addslashes($_POST['mensaje']);
-
-
-
-//Preparamos el mensaje de contacto
+ echo "<p>HOLA $nombre</p> ";
+//Preparo el mensaje de contacto
 $cabeceras = "From: $email\n" //La persona que envia el correo
  . "Reply-To: $email\n";
 $asunto = "Mensaje desde la pagina Web"; //asunto aparecera en la bandeja del servidor de correo
 $email_to = "fausto_zair@hotmail.com"; //cambiar por tu email
-$contenido = "$nombre ha enviado un mensaje desde la web www.movicun.com\n"
+$contenido = "$nombre\n"
 . "\n"
 . "Nombre: $nombre\n"
 . "Email: $email\n"
+. "Sitio Web: $web\n"
 . "Mensaje: $mensaje\n"
 . "\n";
-//Enviamos el mensaje y comprobamos el resultado
+ 
+//Envio
 if (@mail($email_to, $asunto ,$contenido ,$cabeceras )) {
-
+ 
 //Si el mensaje se envía muestra una confirmación
 die("Gracias, su mensaje se envio correctamente.");
 }else{
+ 
 //Si el mensaje no se envía muestra el mensaje de error
 die("Error: Su información no pudo ser enviada, intente más tarde");
 }
