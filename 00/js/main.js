8e8552f7e154af7d270e62b5506819f9401281db
Faustino Loeza <fausto_zair@hotmail.com>
2014-03-03 11:57:21 -0600

    bestbus

--- 00/js/main.js
+++
@@ -1,8 +0,0 @@
-var topDiv = document.getElementById("mueve");
-var speed = 1.5;
-
-window.onscroll = function()
-{
-var yOffset = window.pageYOffset;
-topDiv.style.backgroundPosition = "0px "+ (yOffset / speed) + "px";
-}
