44dc0f41249280fb9631f8d903d20fe56f0fedc9
Faustino Loeza <fausto_zair@hotmail.com>
2014-03-12 10:13:31 -0600

    Reparacion de Slide

--- 00/js/funciones.js
+++ 00/js/funciones.js
@@ -1,32 +1,6 @@
-/*$(function() {
-   	 		$(".rslides").responsiveSlides({
-   	 			auto:true,
-   	 			speed:600,
-   	 		});
-  			});
-	$("a").on("click",function(){
-   if(this.hash){
-      //console.log($(this).attr('href'));
-      $("html body").animate({"scrollTop":$($(this).attr('href')).offset().top-82},1400);
-      return false;
-         }
-});
-
-	$(document).ready(function(){
-	   var $win = window;
-	   var $pos = 40;
-
-	   $(window).scroll(function(){
-	      if($(this).scrollTop() > $pos){
-	         $("a.to-top").css("opacity",".5");
-	         $("header").css("box-shadow","2px 5px 20px rgb(14,62,146,0.5)");
-	      }else{
-	         $("a.to-top").css("opacity","0");
-	         $("header").css("shadow","1px 2px 1px transparent");
-	      }
-	   })
-	});
-
-	$(".to-top").on("click",function(){
-	   $("html, body").animate({scrollTop:0},"fast");
-	});*/
+$(function() {
+     	 		$(".rslides").responsiveSlides({
+     	 			auto:true,
+     	 			speed:400,
+     	 			speed:600,});
+     	 	});
