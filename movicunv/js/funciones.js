ef889f8d5c378702cc42fd95323cc5580450ffd6
faustinoloeza <fausto_zair@hotmail.com>
2014-05-08 13:24:13 -0500

    cambio de index

--- movicunv/js/funciones.js
+++ movicunv/js/funciones.js
@@ -3,13 +3,24 @@ autor: Faustino Loeza Perez
 fecha: 09/2013
 */
 x=$(document);
-x.ready(girarimagenes);
+    x.ready(menu);
+    x.ready(girarimagenes);
   
      
-      function girarimagenes()
+     function girarimagenes()
      {
           x=$('ul#imagenes');
           x.roundabout({btnNext: ".next", duration: 450,reflect:true, btnPrev: ".prev",duration: 450, reflect: false,autoplay: true,autoplayDuration: 5000});
+     }
+     function menu ()
+     {
+        var x=$("#menu");
+          x.click(menumagico);
+     }
+     function menumagico()
+     {
+        var x=$("nav ul");
+        x.slideToggle("slow");
      }
 
     
